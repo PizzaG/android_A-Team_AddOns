4503be9e1fd895a9f054127228fcaf617f238514
PizzaG <49663808+PizzaG@users.noreply.github.com>
2021-05-20 17:39:27 -0500

    Delete FinalZip directory

--- FinalZip/AddOns/post-fs-data.d/CleanUp.sh
+++
@@ -1,9 +0,0 @@
-
-sleep 300
-su -c rm -r data/adb/post-fs-data.d/PostBootSetup.sh
-su -c rm -r data/adb/post-fs-data.d/CleanUp.sh
-su -c rm -r cache/recovery/openrecoveryscript
-su -c rm -r cache/Busybox.zip
-su -c rm -r cache/Magisk.apk
-su -c rm -r cache/Magisk.zip
-exit
